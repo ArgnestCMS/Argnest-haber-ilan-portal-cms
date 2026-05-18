a0731156d4d347115cdf87090f32172e57ddf807
Fikret Kaya <argnest0101@gmail.com>
2026-05-19 01:36:54 +0300

    add echo reverb frontend bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,28 @@
 import axios from 'axios';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Pusher = Pusher;
+
+const reverbKey = import.meta.env.VITE_REVERB_APP_KEY;
+const reverbHost = import.meta.env.VITE_REVERB_HOST || window.location.hostname;
+const reverbPort = import.meta.env.VITE_REVERB_PORT || 8081;
+const reverbScheme = import.meta.env.VITE_REVERB_SCHEME || 'http';
+
+window.Echo = null;
+
+if (reverbKey) {
+    window.Echo = new Echo({
+        broadcaster: 'reverb',
+        key: reverbKey,
+        wsHost: reverbHost,
+        wsPort: reverbPort,
+        wssPort: reverbPort,
+        forceTLS: reverbScheme === 'https',
+        enabledTransports: ['ws', 'wss'],
+    });
+}
